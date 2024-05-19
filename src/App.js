096e26584a365ad7cc4d74a66fe67353dd897685
santiago <santiagonaya190@gmail.com>
2024-05-19 16:09:49 -0300

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Hero from './components/Hero/';
+function App() {
+  return (
+    <div className="App">
+          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+const hero = () => {
+  return (
+<div className="hero">
+   <div className="left-h">left side</div>
+   <div className="right-h">right side</div>
+
+</div>
+  )
+}
+
+export default hero
