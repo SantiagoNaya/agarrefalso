adfbb59fbb9c653e0743b64c0659e5d87434e098
santiago <santiagonaya190@gmail.com>
2024-05-28 18:10:24 -0300

    feat: modifique el css para centrar los elementos agregue 2 botones sin funcionalidad y importe imagenes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Header from '../Header/Header';
 import './Hero.css';
-
+import hero_image from "../../assets/logo.png";
+import hero_image_back from "../../assets/logo.png";
+import Heart from "../../assets/logo.png";
+import Calories from "../../assets/logo.png";
 const Hero = () => {
     return (
         <div className="hero">
@@ -20,11 +23,38 @@ const Hero = () => {
                     <span className='stroke-text'> Entrena</span>
                     <span>Tu</span>
                 </div>
-              <span>
-                  Cuerpo
-                </span>
+                <div>
+                <span> Cuerpo </span>
                 </div>
-            </div>
+                <div>
+                    <span>
+                        Aca te ayudaremos a armar y construir el cuerpo de tu sueños para vivir una vida completa
+                    </span>
+                    </div>
+                </div>
+                     {/* figuras*/}
+             <div className="figures">
+                <div>
+                    <span> +140 </span>
+                    <span> Entrenadores Expertos </span>
+                    </div>
+                <div>
+                    <span> +978 </span>
+                    <span> mienmbros nuevos</span>
+                    </div>
+                <div>
+                    <span> +40 </span>
+                    <span> Programas de entrenamiento </span>
+                    </div>
+             </div>
+           
+             {/* hero buttons*/}
+             <div className="hero-buttons">
+                <button className="btn">Comenza Aca</button>
+                <button className="btn">Aprende Mas</button>
+             </div>
+             </div>
+        
             <div className="right-h"> right side </div>
         </div>
         
