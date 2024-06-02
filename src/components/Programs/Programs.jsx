d4eb9f45c8a4458cf4e841c6b062eb24349af3b0
santiago <santiagonaya190@gmail.com>
2024-06-02 02:27:23 -0300

    feat: segunda seccion programs terminada

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 import './Programs.css'
+import {programsData} from '../../data/programsData'
+import RightArrow from '../../assets/rightArrow.png'
 const Programs =() => {
   return (
     <div className="Program" id="programs">
           {/* header*/}
 <div className="Programs-header">
           <span className='stroke-text'>Explore our</span>
-          <span>
-          Programs
-            </span>
-            <span className='stroke-text'>
-            to shape you
-            </span>
-            </div>
+          <span> Programs</span>
+     <span className='stroke-text'> to shape you</span>
+     </div>
+     <div className="program-categories">
+        {programsData.map((program)=>(
+          <div className="category">
+              {program.image}
+              <span>{program.heading}</span> 
+              <span>{program.details}</span>
+              <div className="join-now"><span>join Now</span><img src={RightArrow} alt="" /></div>
+          </div>
+        ))}
+     
+    
+     </div>    
     </div>
-  )
-}
+    
+  );
+};
 
-export default Programs
+export default Programs;
