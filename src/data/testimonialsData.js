4cdddfde3d30cdcc385d57417b935fe9746a0453
santiago <santiagonaya190@gmail.com>
2025-09-29 17:43:23 -0300

    feat: cambie la descripcion de testimonials data

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -6,15 +6,15 @@ export const testimonialsData = [
   {
     image: image1,
     review:
-      " Santiago es un apasionado del fitness con más de 10 años de experiencia en entrenamiento personal y grupal. Su enfoque innovador y motivador ha ayudado a cientos de personas a alcanzar sus objetivos de salud y bienestar. Alejandro se especializa en entrenamiento funcional y nutrición deportiva, y está comprometido a guiarte en cada paso del camino hacia una vida más activa y saludable.",
-    name: 'Santiago Naya',
-    status : 'COACH'
+      " Joaco es un apasionado del fitness con más de 10 años de experiencia en entrenamiento personal y grupal. Su enfoque innovador y motivador ha ayudado a cientos de personas a alcanzar sus objetivos de salud y bienestar. Alejandro se especializa en entrenamiento funcional y nutrición deportiva, y está comprometido a guiarte en cada paso del camino hacia una vida más activa y saludable.",
+    name: 'Joaquin',
+    status : 'Entrenador'
   },
   {
     image: image2,
     review: 'Agustin un entrenador certificado y amante del yoga con una trayectoria de 8 años en el mundo del fitness. Su filosofía se basa en el equilibrio entre cuerpo y mente, proporcionando clases que combinan fuerza, flexibilidad y relajación. Agustin crea un ambiente acogedor y alentador, ideal para cualquier persona que busque mejorar su salud física y mental  ',
     name: 'Agustin Porto',
-    status: 'COACH'
+    status: 'Entrenador'
   },
   {
     image : image3,
